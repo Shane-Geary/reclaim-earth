b3f19ee2abd1aba18e1fb24e90a1bff3783d2fb6
Shane Geary <shanegeary1994@gmail.com>
2026-01-12 17:28:53 -0700

    Clean up, add ControlButton system

--- Assets/Scripts/Runtime/GameplayUI.cs
+++ Assets/Scripts/Runtime/GameplayUI.cs
@@ -5,61 +5,10 @@ using UnityEngine.UIElements;
 
 public class GameplayUI : MonoBehaviour
 {
-    private UIDocument uiDocument;
-    private VisualElement playerControlButton;
-
-    private WeaponController weaponController;
-    private PlayerController playerController;
-
-    private bool isControlButtonPressed = false;
-    // private bool isControlButtonMoved = false;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        uiDocument = GetComponent<UIDocument>();
-        weaponController = GameManager.Instance.weaponController;
-        playerController = GameManager.Instance.playerController;
-
-        playerControlButton = uiDocument.rootVisualElement.Q<VisualElement>("PlayerControlButton");
-
-        PointerDownEvent pointerDownEvent = new();
-        PointerUpEvent pointerUpEvent = new();
-        playerControlButton.RegisterCallback<PointerDownEvent>(ev => isControlButtonPressed = true);
-        playerControlButton.RegisterCallback<PointerUpEvent>(ev =>
-        {
-            isControlButtonPressed = false;
-            // if (isControlButtonMoved)
-            // {
-            //     isControlButtonMoved = false;
-            //     // playerController.MoveCharacter(Vector2.zero);
-            // }
-        });
-
-        // playerControlButton.RegisterCallback<PointerMoveEvent>(ev => 
-        // {
-        //     isControlButtonMoved = true;
-        //     MoveControlButton(ev.position);
-        // });
-    }
-
-    void Update()
-    {
-        // Weapon firing logic
-        if (Time.time >= weaponController.nextFireTime && isControlButtonPressed)
-        {
-            weaponController.FireWeapon();
-        }
-    }
-
-    private void MoveControlButton(Vector3 position)
-    {
-
-    }
-
-    void OnDestroy()
-    {
-        playerControlButton.UnregisterCallback<PointerDownEvent>(ev => isControlButtonPressed = true);
-        playerControlButton.UnregisterCallback<PointerUpEvent>(ev => isControlButtonPressed = false);
-    }
+    // private UIDocument uiDocument;
+    // // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // void Start()
+    // {
+    //     uiDocument = GetComponent<UIDocument>();
+    // }
 }
