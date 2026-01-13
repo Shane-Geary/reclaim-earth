6ade3038548d9ecf19edcd11ccbeeb10c426fb20
Shane Geary <shanegeary1994@gmail.com>
2026-01-12 18:36:13 -0700

    Using EnhancedTouch from the InputSystem to handle touch input on virtual joystick - weapon fire action working, pending testing

--- Assets/Scripts/Runtime/ControlButton.cs
+++ Assets/Scripts/Runtime/ControlButton.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using ETouch = UnityEngine.InputSystem.EnhancedTouch;
 
 public class ControlButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private RectTransform rectTransform;
+
+    private Finger MovementFinger;
+    private Vector2 MovementAmount;
+
+    private WeaponController weaponController;
+
+    private bool isFingerDown = false;
+
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+        ETouch.Touch.onFingerDown += OnTouchFingerDown;
+        ETouch.Touch.onFingerUp += OnTouchFingerUp;
+        ETouch.Touch.onFingerMove += OnTouchFingerMove;
+    }
+
     void Start()
+    {
+        weaponController = GameManager.Instance.weaponController;
+    }
+
+    void Update()
+    {
+        if (Time.time >= weaponController.nextFireTime && isFingerDown)
+        {
+            weaponController.FireWeapon();
+        }
+    }
+
+    private void OnTouchFingerDown(Finger TouchedFinger)
+    {
+        Debug.Log("Touch Finger Down");
+        isFingerDown = true;
+
+        MovementFinger = TouchedFinger;
+        MovementAmount = Vector2.zero;
+    }
+
+    private void OnTouchFingerUp(Finger obj)
+    {
+        isFingerDown = false;
+    }
+
+    private void OnTouchFingerMove(Finger obj)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        ETouch.Touch.onFingerDown -= OnTouchFingerDown;
+        ETouch.Touch.onFingerUp -= OnTouchFingerUp;
+        ETouch.Touch.onFingerMove -= OnTouchFingerMove;
+        EnhancedTouchSupport.Disable();
     }
 }
